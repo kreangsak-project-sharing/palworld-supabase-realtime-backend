b6406df465a1135afb6dd84be099c78568cf22ea
Kreangsak <128896272+kreangsak-project-sharing@users.noreply.github.com>
2024-05-07 01:03:51 +0700

    Update config.ts

--- prisma/config.ts
+++ prisma/config.ts
@@ -68,33 +68,60 @@ export async function addSelectPolicy() {
   try {
     for (const table of tables) {
       const existingPolicies: any[] = await prisma.$queryRaw`
-      SELECT polname::text as polname
-      FROM pg_catalog.pg_policy
-      WHERE polname = ${"select_anon_" + table + "_policy"}
-      AND polrelid = (SELECT oid FROM pg_catalog.pg_class WHERE relname = ${table})
-    `;
-
-      // console.log(existingPolicies);
+        SELECT polname::text as polname
+        FROM pg_catalog.pg_policy
+        WHERE polname = ${"select_anon_" + table + "_policy"}
+        AND polrelid = (SELECT oid FROM pg_catalog.pg_class WHERE relname = ${table})
+      `;
 
       if (existingPolicies.length === 0) {
         await prisma.$executeRawUnsafe(`
-        create policy "policy_name"
-        on "public"."realtime_loginrecord"
-        as PERMISSIVE
-        for SELECT
-        to anon
-        using (
-          true
-          );
+          CREATE POLICY "select_anon_${table}_policy"
+          ON "public"."${table}"
+          AS PERMISSIVE
+          FOR SELECT
+          TO anon
+          USING (true);
         `);
 
-        console.log(
-          "Select policies for authenticated users added successfully."
-        );
+        console.log(`Select policy for table ${table} added successfully.`);
       }
     }
-  } catch (error) {
+    console.log("Select policies for authenticated users added successfully.");
+  } catch (error: any) {
     console.error("Error adding select policies:", error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+//
+// exposedSchemas
+export async function exposedSchemas() {
+  try {
+    const roles = ["anon", "authenticated", "service_role"];
+
+    for (const role of roles) {
+      const result: any[] = await prisma.$queryRaw`
+        SELECT has_schema_privilege(${role}, 'public', 'USAGE') AS has_privilege
+      `;
+
+      if (!result[0]?.has_privilege) {
+        await prisma.$executeRawUnsafe(
+          `GRANT USAGE ON SCHEMA public TO ${role};`
+        );
+        await prisma.$executeRawUnsafe(
+          `GRANT ALL ON ALL TABLES IN SCHEMA public TO ${role};`
+        );
+        console.log(`Permission granted to role ${role} for schema public.`);
+      } else {
+        console.log(`Role ${role} already has permission for schema public.`);
+      }
+    }
+
+    console.log("Schemas exposed successfully.");
+  } catch (error) {
+    console.error("Error adding exposed schemas:", error);
   } finally {
     await prisma.$disconnect();
   }
